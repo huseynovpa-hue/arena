47157b7abb443372c8c718ebdcfdca75e6b2c6d2
huseynovpa-hue <huseynov.pa@gmail.com>
2026-08-07 11:07:54 +0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,36 @@
+"use client";
+import Link from "next/link";
 
+export default function Footer() {
+  return (
+    <footer style={{ background: "#0b1120", borderTop: "1px solid #1c2a48", padding: "20px 16px", marginTop: "auto" }}>
+      <div style={{ maxWidth: "672px", margin: "0 auto" }}>
+        {/* Links */}
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginBottom: "12px", flexWrap: "wrap" }}>
+          <Link href="/about" style={{ fontSize: "12px", color: "#6a7c9a", textDecoration: "none" }}>
+            About Us
+          </Link>
+          <Link href="/about" style={{ fontSize: "12px", color: "#6a7c9a", textDecoration: "none" }}>
+            Contact
+          </Link>
+          <Link href="/privacy" style={{ fontSize: "12px", color: "#6a7c9a", textDecoration: "none" }}>
+            Privacy Policy
+          </Link>
+          <Link href="/rules" style={{ fontSize: "12px", color: "#6a7c9a", textDecoration: "none" }}>
+            Rules
+          </Link>
+        </div>
+
+        {/* Brand */}
+        <div style={{ textAlign: "center" }}>
+          <div style={{ fontSize: "13px", fontWeight: 800, color: "#22c55e", letterSpacing: "2px", marginBottom: "4px" }}>
+            ⚽ ARENA
+          </div>
+          <div style={{ fontSize: "10px", color: "#4a5568" }}>
+            © {new Date().getFullYear()} Arena Football Predictions. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
